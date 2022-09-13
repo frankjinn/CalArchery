4eef5ca70e95fb195a12b22963f823dd8eec1ff0
Frank Jin <frank.jin@berkeley.edu>
2022-09-13 00:44:25 -0700

    Added section text and changed styling

--- src/components/HomeComp/SplashGrid.js
+++ src/components/HomeComp/SplashGrid.js
@@ -39,26 +39,34 @@ function GridMap() {
           <div className={`${style.bgImage} ${style.img1}`}>
             <div className={style.body}>
               <h1 className={style.title} style = {{color : "#edc25c"}}>Who We Are</h1>
-              <Text className={style.desc}>
-                Cal Archery is one of the first collegiate archery teams to be organized in the 
-                west coast. Our team travels around the country competing in collgiate level archery
+              <Text className={style.desc} style = {{color : "white", fontWeight : "700"}}>
+                We're a community of students passionate about archery. Made out of barebow, olympic recurve, and compound archers, we compete nationally in various competitions with east coast universities and other universities around the country. 
               </Text>
               <Button 
               colorScheme='yellow' 
               variant='solid'>
-                <a href="http://localhost:3000/about">Learn More</a>
+                <a href="http://localhost:3000/about">More About Us</a>
               </Button>
             </div>
           </div>
-          
-          <div className={`${style.bgImage} ${style.img1}`}>
+
+          <div className={`${style.bgImage}`} style = {{backgroundColor : "white"}}>
+            <div className={style.body}>
+              <h1 className={style.title} style = {{color : "grey"}}>Our History</h1>
+              <Text className={style.desc}>
+                Cal Archery was one of the 1st recreational collegiate archery teams to become established on the west coast. Our team has lead the Western Region in competitiveness and we've also helped a large number of other west coast teams get started. Cal Archery began about 15 years ago and we've grown tremendously ever since. Our team is always excited to accept new club members, whether or not you are a beginner or a very advanced archer. We provide all necessary equipment, which supports archers of all levels and skills.</Text>
+            </div>
+          </div>
+
+          <div className={`${style.bgImage} ${style.img2}`}>
             <div className={style.body}>
               <h1 className={style.title} style = {{color : "#4169e1"}}>New to Archery?</h1>
-              <Text className={style.desc}>Stuff about Ascent</Text>
+              <Text className={style.desc} style = {{color : "white", fontWeight : "700"}}>
+                Run in the first two months of the semester, you can learn how to shoot from the ground up! ASCENT is a beginner program where our most advanced archers and coaches help you improve your shooting skills. All equipment is provided, and you'll be starting off with lots of friends who are also just beginning to shoot. </Text>
               <Button 
               colorScheme='blue' 
               variant='solid'>
-                <a href="http://localhost:3000/ascent">Learn More</a>
+                <a href="http://localhost:3000/ascent">ASCENT</a>
               </Button>
             </div>
           </div>
